fdef6e65cfda56b8972d2a8825136d4a8b7459f0
Ben <16917183+bencrts@users.noreply.github.com>
2024-09-03 13:44:30 +0100

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,47 @@
+sage raw_data.py 128 matzov 32 test32 binary
+sage raw_data.py 128 matzov 32 test32 ternary
+sage raw_data.py 128 matzov 32 test32 gaussian
+
+sage raw_data.py 192 matzov 32 test32 binary
+sage raw_data.py 192 matzov 32 test32 ternary
+sage raw_data.py 192 matzov 32 test32 gaussian
+
+sage raw_data.py 256 matzov 32 test32 binary
+sage raw_data.py 256 matzov 32 test32 ternary
+sage raw_data.py 256 matzov 32 test32 gaussian
+
+sage raw_data.py 128 matzov 64 test64 binary
+sage raw_data.py 128 matzov 64 test64 ternary
+sage raw_data.py 128 matzov 64 test64 gaussian
+
+sage raw_data.py 192 matzov 64 test64 binary
+sage raw_data.py 192 matzov 64 test64 ternary
+sage raw_data.py 192 matzov 64 test64 gaussian
+
+sage raw_data.py 256 matzov 64 test64 binary
+sage raw_data.py 256 matzov 64 test64 ternary
+sage raw_data.py 256 matzov 64 test64 gaussian
+
+sage raw_data.py 128 quantum 32 test32 binary
+sage raw_data.py 128 quantum 32 test32 ternary
+sage raw_data.py 128 quantum 32 test32 gaussian
+
+sage raw_data.py 192 quantum 32 test32 binary
+sage raw_data.py 192 quantum 32 test32 ternary
+sage raw_data.py 192 quantum 32 test32 gaussian
+
+sage raw_data.py 256 quantum 32 test32 binary
+sage raw_data.py 256 quantum 32 test32 ternary
+sage raw_data.py 256 quantum 32 test32 gaussian
+
+sage raw_data.py 128 quantum 64 test64 binary
+sage raw_data.py 128 quantum 64 test64 ternary
+sage raw_data.py 128 quantum 64 test64 gaussian
+
+sage raw_data.py 192 quantum 64 test64 binary
+sage raw_data.py 192 quantum 64 test64 ternary
+sage raw_data.py 192 quantum 64 test64 gaussian
+
+sage raw_data.py 256 quantum 64 test64 binary
+sage raw_data.py 256 quantum 64 test64 ternary
+sage raw_data.py 256 quantum 64 test64 gaussian
